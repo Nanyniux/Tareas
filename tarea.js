68aac494a1bcae396435dcf06b07a944661f8557
minhaconta9173@gmail.com <minhaconta9173@gmail.com>
2022-03-30 22:01:32 -0300

    Tarea laço for

--- tarea.js
+++ tarea.js
@@ -1,1 +1,10 @@
-console.log('Oi mundo')
+var Beleza=['primer','serum','corrector','iluminador','contorno','barra','polvo']
+
+for(var T=0;T<Beleza.length;T=T+1){console.log('llevar en tu bolsa ' + Beleza[T])
+if(T%2==0){console.log("mensaje especial n° par")
+if(Beleza[T]=="polvo"){console.log('final especial')
+if(4<3 ^ 6==6){console.log("Bom dia")}}}}
+
+
+for(var element of Beleza){console.log(element);
+if(element.includes("e")){console.log("Hola Calixto")}}
